0dcd3aa225da138985c735075dac45999efb2796
ivanmonzon <ivomonzon@gmail.com>
2022-05-18 18:27:44 -0300

    un commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import { commerce } from './lib/commerce';
+import { Products, Navbar, Cart } from './components';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+const App = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  }
+
+  const fetchCart = async () => {
+    
+    setCart(await commerce.cart.retrieve());
+  }
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+    
+    setCart(item.cart);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+  }, []);
+
+
+  return (
+    <Router>
+      <div>
+        <Navbar totalItems={cart.total_items}/> 
+        <Routes>
+          <Route exact path="/">
+            <Products products={products} onAddToCart={handleAddToCart}/>
+          </Route>
+          <Route exact path="/cart">
+            <Cart cart={cart}/>
+          </Route>  
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
